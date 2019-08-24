a1d64f4279d1db8d06be8b7ad789b3c93f4321d2
László Horváth <bedafoltyn@gmail.com>
2019-08-24 14:23:14 +0200

    refactor: dependencies abstracted

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-const rss = require('./lib/rss')
-const article = require('./lib/article')
-const weather = require('./lib/weather')
-const rate = require('./lib/rate')
+const fetch = require('node-fetch')
+const cheerio = require('cheerio')
+const Parser = require('rss-parser')
+const parser = new Parser()
 
-module.exports = {
-  scrapeRSS: rss.scrape,
-  scrapeArticle: article.scrape,
-  scrapeWeather: weather.scrape,
-  scrapeRate: rate.scrape
+const HTTPClient = require('./lib/utils/http_client')(fetch)
+const HTMLParser = require('./lib/utils/html_parser')(cheerio)
+const RSSParser = require('./lib/utils/rss_parser')(parser)
+const Article = require('./lib/article')(HTTPClient, HTMLParser)
+const RSS = require('./lib/rss')(RSSParser)
+const Weather = require('./lib/weather')(HTTPClient)
+const Rate = require('./lib/rate')(HTTPClient, HTMLParser)
+
+module.exports = function () {
+  return {
+    RSS: RSS,
+    Article: Article,
+    Weather: Weather,
+    Rate: Rate
+  }
 }
