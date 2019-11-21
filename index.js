a404dacd12ddc1ebe6ef8dbc9bd040f52298d61a
László Horváth <bedafoltyn@gmail.com>
2019-11-21 22:16:11 +0100

    gemius scraper added

--- index.js
+++ index.js
@@ -10,12 +10,18 @@ const Article = require('./lib/article')(HTTPClient, HTMLParser)
 const RSS = require('./lib/rss')(RSSParser)
 const Weather = require('./lib/weather')(HTTPClient)
 const Rate = require('./lib/rate')(HTTPClient, HTMLParser)
+const Statistics = require('./lib/statistics')(HTTPClient)
 
 module.exports = function () {
   return {
     RSS: RSS,
     Article: Article,
     Weather: Weather,
-    Rate: Rate
+    Rate: Rate,
+    Statistics: Statistics
   }
 }
+
+/* Statistics.Gemius.scrape('https://rating.gemius.com/hu/reports/segmentation/data/table?&&selectedMetrics%5B0%5D%5Bformat%5D%5B%5D=integer&selectedPeriod%5BstartDate%5D=', ['index.hu', 'origo.hu'])
+  .then(console.log)
+  .catch(console.log) */
