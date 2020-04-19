e29f266799610773111dc3952322dfd11837a183
László Horváth <bedafoltyn@gmail.com>
2020-04-19 12:20:22 +0200

    character decoding added before html parsing, non-utf-8 bug fixed

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const fetch = require('node-fetch')
-const cheerio = require('cheerio')
-const Parser = require('rss-parser')
-const parser = new Parser()
-const {PythonShell} = require('python-shell')
+const fetch = require("node-fetch");
+const cheerio = require("cheerio");
+const Parser = require("rss-parser");
+const parser = new Parser();
+const { PythonShell } = require("python-shell");
 
-const HTTPClient = require('./lib/utils/http_client')(fetch)
-const HTMLParser = require('./lib/utils/html_parser')(cheerio)
-const RSSParser = require('./lib/utils/rss_parser')(parser)
-const Article = require('./lib/article')(HTTPClient, HTMLParser)
-const RSS = require('./lib/rss')(RSSParser)
-const Weather = require('./lib/weather')(HTTPClient)
-const Rate = require('./lib/rate')(HTTPClient, HTMLParser)
-const Statistics = require('./lib/statistics')(HTTPClient, HTMLParser)
+const HTTPClient = require("./lib/utils/http_client")(fetch);
+const HTMLParser = require("./lib/utils/html_parser")(cheerio);
+const RSSParser = require("./lib/utils/rss_parser")(parser);
+const Article = require("./lib/article")(HTTPClient, HTMLParser);
+const RSS = require("./lib/rss")(RSSParser);
+const Weather = require("./lib/weather")(HTTPClient);
+const Rate = require("./lib/rate")(HTTPClient, HTMLParser);
+const Statistics = require("./lib/statistics")(HTTPClient, HTMLParser);
 
 module.exports = function () {
   return {
@@ -19,6 +19,6 @@ module.exports = function () {
     Article: Article,
     Weather: Weather,
     Rate: Rate,
-    Statistics: Statistics
-  }
-}
+    Statistics: Statistics,
+  };
+};
